fdb139fbd7feeb9f9eeb28f02e92af5315fb4a15
Jeffrey Stanton <jffry@users.noreply.github.com>
2020-08-13 23:15:56 -0400

    Auth helper returns null if no auth file is present, instead of throwing an error

--- src/auth.js
+++ src/auth.js
@@ -4,8 +4,12 @@ const path = require('path');
 const AUTH_FILE = path.join(__dirname, '../data/auth.json');
 
 // Loads auth each time to make it easier to dynamically update to fix problems
-exports.getAuth = function(name) {
-  let json = fs.readFileSync(AUTH_FILE).toString();
-  let data = JSON.parse(json);
-  return data[name];
+exports.getAuth = function (name) {
+  if (fs.existsSync(AUTH_FILE)) {
+    let json = fs.readFileSync(AUTH_FILE).toString();
+    let data = JSON.parse(json);
+    return data[name];
+  } else {
+    return null;
+  }
 }
